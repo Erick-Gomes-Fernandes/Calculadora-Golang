5b2a4be609f812f07dcde9e9acd76e78fd47e6cc
Erick Fernandes <erick.gomes.fernandes.egf@gmail.com>
2026-03-02 13:00:55 -0300

    Criado funções de soma, subtração, multiplicação e divisão

--- main.go
+++ main.go
@@ -2,6 +2,35 @@ package main
 
 import "fmt"
 
+var resultado int
+
 func main() {
-	fmt.Println("Repositório Iniciado")
+	fmt.Println("Resultado:", resultado)
+	resultado = soma(5)
+	fmt.Println("Resultado:", resultado)
+	resultado = subtrai(3)
+	fmt.Println("Resultado:", resultado)
+	resultado = multiplica(4)
+	fmt.Println("Resultado:", resultado)
+	resultado = divide(2)
+	fmt.Println("Resultado:", resultado)
 }
+
+func soma(x int) int {
+	return (resultado + x)
+}
+
+func subtrai(x int) int {
+	return (resultado - x)
+}
+
+func multiplica(x int) int {
+	return (resultado * x)
+}
+
+func divide(x int) int {
+	return (resultado / x)
+}
+
+// Operações Soma (+), Subtração (-), Multiplicação (*), Divisão (/)
+// Rodar enquanto quiser rodar, dar opção de finalizar o programa ao usuário
