9d55ceaa9b7c3d2075dbfcab90fa1ea61ecde493
Erick Fernandes <erick.gomes.fernandes.egf@gmail.com>
2026-03-03 19:15:08 -0300

    Criado calculadora funcional, faltando polimento

--- main.go
+++ main.go
@@ -2,18 +2,44 @@ package main
 
 import "fmt"
 
-var resultado int
+var resultado, numero int
 
 func main() {
-	fmt.Println("Resultado:", resultado)
-	resultado = soma(5)
-	fmt.Println("Resultado:", resultado)
-	resultado = subtrai(3)
-	fmt.Println("Resultado:", resultado)
-	resultado = multiplica(4)
-	fmt.Println("Resultado:", resultado)
-	resultado = divide(2)
-	fmt.Println("Resultado:", resultado)
+	var opcao int
+
+	for {
+		fmt.Println("Número atual:", resultado)
+		fmt.Println("Escolha um número:")
+		_, err := fmt.Scanln(&numero)
+		if err != nil {
+			fmt.Println("Entrada inválida! Digite apenas números inteiros.")
+			continue
+		}
+
+		fmt.Println("Escolha uma operação pelo indíce:")
+		fmt.Println("1 => Soma")
+		fmt.Println("2 => Subtração")
+		fmt.Println("3 => Multiplicação")
+		fmt.Println("4 => Divisão")
+		fmt.Println("0 => Encerrar programa")
+		fmt.Scanln(&opcao)
+
+		switch opcao {
+		case 1:
+			resultado = soma(numero)
+		case 2:
+			resultado = subtrai(numero)
+		case 3:
+			resultado = multiplica(numero)
+		case 4:
+			resultado = divide(numero)
+		case 0:
+			fmt.Println("Resultado:", resultado)
+			return
+		default:
+			fmt.Println("Opção inválida")
+		}
+	}
 }
 
 func soma(x int) int {
